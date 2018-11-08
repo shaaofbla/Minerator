7c23edd6d76c966e2b18a84a84189b76de507bd3
Shaaofbla <daniel.b.schmocker@gmail.com>
2018-11-08 10:59:06 +0100

    saver modul is almost done, midi file not correctely saved

--- modules/saver.py
+++ modules/saver.py
@@ -1,14 +1,16 @@
 
 import os
 import re
+from shutil import copyfile
 from modules.midiUtils import chopPrimer
+from modules.fileUtils import createDir
 
 
 
 class saver():
     def __init__(self, parent):
         self.configSaver(parent)
-        self.porjectName = parent.projectName
+        self.projectName = parent.projectName
         self.saveDir = parent.saveDir
 
     def configSaver(self, parent):
@@ -23,14 +25,34 @@ class saver():
 
     def moveMidiToTarget(self):
         self.processPathes()
+        self.copyMidiFiles()
+    
+    def copyMidiFiles(self):
+        #self.createTargedDirs()
+        for midi, targed in zip(self.midiFiles, self.targedFile):
+            self.createTargedDir(targed)
+            copyfile(midi, targed)
+    
+    def createTargedDir(self, targed):
+        midiFilePattern = re.compile("(.*[/]).*.mid")
+        targed = midiFilePattern.findall(targed)[0]
+        os.makedirs(targed)
 
     def processPathes(self):
-        print self.midiFiles
         jobNamesPattern = re.compile(".*GeneratorOut[/](.*)[/].*[/].*.mid")
         newlist = []
-        for match in self.midiFiles:
-            newlist.append(jobNamesPattern.findall(match)[0])
-        print "re Out", set(newlist)
+        for midiFile in self.midiFiles:
+            newlist.append(jobNamesPattern.findall(midiFile)[0])
+        uniqueJobnames = list(set(newlist))
+        self.uniqueJobnames = uniqueJobnames
+        targedFile = []
+        for midiFile in self.midiFiles:
+            for i, name in enumerate(uniqueJobnames):
+                if midiFile.find(name)!= -1:
+                    targedFile.append(midiFile.replace(name, "JobNr{}".format(i)))
+        for i, targed in enumerate(targedFile):
+            targedFile[i] = targed.replace(self.tempDir, "./{}/".format(self.projectName))
+        self.targedFile = targedFile
         
     def removePrimerMidiNotes(self):
         self.processed_midi = []
@@ -41,8 +63,8 @@ class saver():
     def getTempMidiFiles(self):
         jobdirs = self.getJobDir()
         self.jobNames = jobdirs
-        modeldirs = self.getModelDirs(jobdirs)
-        self.getMidiFilePath(modeldirs)
+        self.modeldirs = self.getModelDirs(jobdirs)
+        self.getMidiFilePath(self.modeldirs)
 
     def getMidiFilePath(self, modelDirs):
         midiFiles = []
